fdd2aaa4ced3fcf8d24cd010d3e62f19a9287b2e
Jameson Quinn <jameson.quinn+github@gmail.com>
2018-05-22 09:14:16 -0400

    Old incomplete work on nonnormal.jl

---
+++ src/nonnormal.jl
@@ -0,0 +1,122 @@
+module bkf
+  using Distributions
+  using StatsBase
+  using Memoize
+  using Compat
+  import Base.length
+
+  abstract KalmanFilter
+
+  abstract LinearKalmanFilter <: KalmanFilter
+
+  abstract Model
+
+  abstract ObservationModel
+
+  abstract AbstractState
+
+  type State{T} <: AbstractState
+      x::Vector{T}
+      p::Matrix  #covariance
+  end
+
+  Base.(:(==))(x1::State,x2::State) = x1.x==x2.x && x1.p == x2.p
+
+  type LinearModel <: Model
+      a::Matrix #state transition matrix
+      g::Matrix #process noise to state
+      q::Matrix #process noise covariance (hopefully diagonal)
+  end
+
+  ## This is the first function a Kalman filter needs to
+  # implement: Apply its model to its state to generate a
+  # new state
+
+  function ap(f::LinearModel,x::State)
+      x1 = f.a*x.x
+      p1 = f.a*x.p*f.a' + f.g*f.q*f.g'
+      State(x1,p1)
+  end
+
+  type Observation{T}
+      y::Vector{T}
+  end
+
+#  Base.convert(::Type{Observation},y) = Observation([y])
+
+  type LinearObservationModel <: ObservationModel
+      h::Matrix #connects state to observations; hopefully diagonal
+      r::Matrix #observation covariance
+  end
+
+  function LinearObservationModel(h::Matrix)
+      LinearObservationModel(h,eye(size(h,1)))
+  end
+
+
+  type BasicKalmanFilter <: LinearKalmanFilter
+      x::State
+      f::LinearModel
+      z::LinearObservationModel
+  end
+
+  Base.copy(kf::KalmanFilter) = deepcopy(kf)
+
+  ## This is the second of two functions a Kalman filter needs to implement
+  # Return three matrices:
+  # 1. The residual (y-Hx) (`res`)
+  # 2. The cross-covariance between state and measurement (`ph`)
+  # 3. The innovation covariance matrix
+  function covs(kf::BasicKalmanFilter,y::Observation)
+      res = y.y - kf.z.h * kf.x.x
+      ph = kf.x.p * kf.z.h'
+      s = kf.z.h * ph + kf.z.r
+      (res,ph,s)
+  end
+
+ function toDistribution(kf::BasicKalmanFilter)
+    MvNormal(kf.x.x,((kf.x.p + kf.x.p') / 2))
+  end
+
+    function forwardDistribution(m::LinearModel,x::Vector,r::Range)
+        #print("aaa",noiseMatrix(m)[1:2,1:2],"\n")
+        #print("bbb",noiseMatrix(m)[r,r],"\n")
+        #print("ccc",x[1:2],"\n")
+        #print("ddd",x[r],"\n")
+
+      MvNormal(x[r],noiseMatrix(m)[r,r])
+    end
+
+    function forwardDistribution(m::LinearModel,x::Float64,l::Int64)
+        #print("aaa",noiseMatrix(m)[1:2,1:2],"\n")
+        #print("bbb",noiseMatrix(m)[r,r],"\n")
+        #print("ccc",x[1:2],"\n")
+        #print("ddd",x[r],"\n")
+
+      Normal(x,noiseMatrix(m)[l,l])
+    end
+
+    @memoize function noiseMatrix(m::LinearModel)
+        m.g * m.q * m.g'
+    end
+
+    function noiseDistribution(kf::BasicKalmanFilter)
+      MvNormal(noiseMatrix(kf.f))
+    end
+
+    function obsNoiseDistribution(kf::BasicKalmanFilter)
+      MvNormal(kf.z.r)
+    end
+
+    function obsNoiseDistribution(kf::BasicKalmanFilter, fromI, toI)
+      MvNormal(kf.z.r[fromI:toI,fromI:toI])
+    end
+
+
+include("particlefilter.jl")
+include("frankenfilter.jl")
+include("finkel.jl")
+
+  include("filter.jl")
+  include("unscented.jl")
+end
---
+++ src/oldfinkel.jl
@@ -0,0 +1,228 @@
+#included in bkf.jl
+
+include("delegatemacro.jl")
+
+abstract AbstractSparseFilter <: KalmanFilter
+
+type SparseKF <: AbstractSparseFilter
+    f::BasicKalmanFilter
+    neighbors::Vector{Vector{Int16}}
+end
+
+
+
+
+
+
+@delegate SparseKF.f [ covs, toDistribution, noiseDistribution, obsNoiseDistribution ]
+
+function SparseKF(f::BasicKalmanFilter) #assumes neighborliness is symmetric
+    neighbors = Vector{Vector{Int16}}()
+    m = f.f.a
+    for j = 1:size(m,2)
+        nset = Vector{Int16}()
+        for i = 1:size(m,1)
+            if m[i,j] != 0
+                push!(nset, i)
+            end
+        end
+        push!(neighbors, nset)
+    end
+    SparseKF(f,neighbors)
+end
+
+abstract AbstractFinkel <: AbstractParticleFilter
+
+type FinkelToe{T,F<:KalmanFilter} <: AbstractFinkel
+    tip::ParticleSet{T,F}
+end
+
+function particleMatrix(fp::AbstractFinkel)
+    fp.tip.particles
+end
+
+function getbkf(fp::AbstractFinkel)
+    fp.tip.filter
+end
+
+type FinkelParticles{T,F<:KalmanFilter} <: AbstractFinkel
+    tip::ParticleSet{T,F} #This is where we do MCMC and get the answer. Also holds the filter. It's got extra stuff; ignore.
+    base::Array{T,2} #This is the raw 1-step progression from last time.
+                #As with all similar arrays, size is [d,n]; that is, first index is space and second is particle.
+        #[space, particle]
+    prev::AbstractFinkel
+    stem::Array{Int64,2} #tells which base each tip comes from
+        #[space, particle]
+    ws::Vector{WeightVec} #selection probabilities at each point; p(y_l|x^i_l)
+        #[space][particle]
+    #lps::Array{Nullable{Float64},2} #log probabilities from base to tip; size [n, n]
+    means::Array{T,2} #base, progressed, without adding noise.
+    prevProbs::Array{Nullable{Float64},2} #sum over neighborhood history of local probability - avoid double calculation
+    localDists::Array{Distribution,2} #for calculating probs
+    totalProb::Array{Float64,2} #convergence diagnostic
+end
+
+function FinkelParticles(prev::AbstractFinkel)
+    d, n = size(particleMatrix(prev))
+    print(d," dn ",n,"\n")
+    tip = ap(prev.tip)
+    stem = zeros(Int64,d,n)
+    filt = getbkf(prev)
+    for j = 1:n
+        for i = 1:d
+            stem[i,j] = j
+        end
+    end
+    fp = FinkelParticles(tip,
+                copy(tip.particles), #base
+                prev,
+                stem,
+                Vector{WeightVec}(), #empty weights
+                filt.f.a * particleMatrix(prev),
+                Array{Nullable{Float64},2}(d,n),
+                Array{Distribution,2}(d,n),
+                zeros(d,n),
+                )
+    getDists!(fp, d, n)
+    calcPrevProbs!(fp, d, n)
+    fp
+end
+
+function particles(fp::FinkelParticles)
+    fp.tip.particles
+end
+
+function reweight!(fp::FinkelParticles, y::Observation)
+    fp.ws = Vector{WeightVec}()
+    diffs = fp.base - fp.tip.filter.z.h * repeat(y.y,outer=[1,fp.tip.n])  # fp.tip.f.z.h should probably be eye ?
+    vars = diag(fp.tip.filter.z.r) #assumes fp.tip.f.z.r is diagonal
+    for i = 1:size(fp.base,1)
+        wvec = Vector{Float64}(fp.tip.n)
+        for j = 1:fp.tip.n
+            wvec[j] = exp(-diffs[i,j]^2 / vars[i] / 2)
+        end
+        push!(fp.ws,WeightVec(wvec))
+    end
+end
+
+function getDists!(fp::FinkelParticles, d, n)
+    for i in 1:n
+        x = Vector(slice(fp.means,:,i))
+        for l in 1:d
+            fp.localDists[l,i] = forwardDistribution(getbkf(fp).f,x,max(l-1,1):min(l+1,d))
+        end
+    end
+end
+
+
+function probSum(fp::FinkelParticles,l::Integer,i::Integer,p::Integer,
+            neighborhood::Range,
+            centerOnly = false)
+    ps = 0.
+    prop = fp.tip.particles[neighborhood,i]
+    hist = fp.stem[neighborhood,i]
+    if p != fp.stem[l,i]
+        if l==1
+            place=1
+        else
+            place=2
+        end
+        hist[place] = p
+        prop[place] = fp.base[l,p]
+    end
+    if centerOnly
+        histSet = Set(p)
+    else
+        histSet = Set(hist)
+    end
+    for h in histSet
+        aprob = pdf(fp.localDists[l,h],prop)
+        for n in neighborhood
+            if n != l
+                aprob /= pdf(forwardDistribution(getbkf(fp).f,
+                                        fp.means[n,h],
+                                        n),
+                                fp.tip.particles[n,i])
+                        #prob, at point n, of going from particle h at time t-1 to
+                        #value fp.tip.particles[n,i] at time t.
+            end
+        end
+        ps += aprob
+    end
+    ps
+end
+
+function probSum(fp::FinkelParticles,l::Integer,i::Integer,p::Integer,d::Int64,
+                centerOnly = false)
+
+    probSum(fp,l,i,p,
+            max(l-1,1):min(l+1,d),centerOnly
+            )
+end
+
+function probSum(fp::FinkelParticles,l::Integer,i::Integer,d::Int64,
+            centerOnly::Bool = false)
+    probSum(fp,l,i,fp.stem[l,i],d,centerOnly)
+end
+
+
+function calcPrevProbs!(fp::FinkelParticles, d, n)
+
+    for i in 1:n
+        for l in 1:d
+            fp.prevProbs[l,i] = Nullable{Float64}()#Nullable(probSum(fp,l,i,d))
+        end
+    end
+end
+
+function mcmc!(fp::FinkelParticles,i::Integer,steps::Integer)
+    d = size(fp.base,1)
+    for s in 1:steps
+        order = randperm(d)
+        for l in order
+            p = sample(fp.ws[l])
+            if p != fp.stem[l,i]
+                neighborhood = max(l-1,1):min(l+1,d)
+                oldSet = Set(fp.stem[i] for i in neighborhood)
+                baseNewProb = newProb = probSum(fp,l,i,p,neighborhood)
+                oldProb = fp.prevProbs[l,i]
+                if isnull(oldProb)
+                    oldProb = probSum(fp, l, i, fp.stem[l,i], neighborhood)
+                    fp.prevProbs[l,i] = Nullable(oldProb)
+                else
+                    oldProb = get(oldProb)
+                end
+                if !(p in oldSet)
+                    newProb += probSum(fp,l,i,fp.stem[l,i],neighborhood,true)
+                    oldProb += probSum(fp,l,i,p,neighborhood,true)
+                end
+                if newProb < oldProb && rand() > (newProb / oldProb)
+                    fp.totalProb[l,i] += newProb / oldProb
+                    continue #M-H rejection
+                end
+                #M-H accepted
+                fp.totalProb[l,i] += 1
+                fp.stem[l,i] = p
+                fp.tip.particles[l,i] = fp.base[l,p]
+                fp.prevProbs[l,i] = baseNewProb
+                for n in neighborhood
+                    if n != l
+                        fp.prevProbs[n,i] = Nullable{Float64}()
+                    end
+                end
+            end
+        end
+    end
+end
+
+function FinkelParticles(prev::AbstractFinkel, y::Observation, nIter=15, debug=true)
+    fp = FinkelParticles(prev)
+    reweight!(fp, y)
+    for i in 1:fp.tip.n
+        mcmc!(fp,i,nIter)
+        if debug
+            print("Ran particle ", i, "; mean tp = ", mean(fp.totalProb[:,1:i]), "\n")
+        end
+    end
+    fp
+end
