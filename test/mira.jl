15477ae71eca7e4703c909f9c8e776a144d15a6f
Jameson Quinn <jameson.quinn+github@gmail.com>
2019-01-27 18:24:51 -0500

    mira already installed, comment out

--- test/mira.jl
+++ test/mira.jl
@@ -1,18 +1,18 @@
-using Pkg
-Pkg.add("LinearAlgebra")
-Pkg.add("Distributions")
-Pkg.add("StatsBase")
-Pkg.add("Memoize")
-Pkg.add("Compat")
-Pkg.add("ForwardDiff")
-Pkg.add("DataFrames")
-Pkg.add("CSV")
-Pkg.add("DataStructures")
-Pkg.add("Revise")
-#Pkg.add("Plots")
-#Pkg.add("Plotly")
-#Pkg.add("Gadfly")
-Pkg.add("NaNMath")
+# using Pkg
+# Pkg.add("LinearAlgebra")
+# Pkg.add("Distributions")
+# Pkg.add("StatsBase")
+# Pkg.add("Memoize")
+# Pkg.add("Compat")
+# Pkg.add("ForwardDiff")
+# Pkg.add("DataFrames")
+# Pkg.add("CSV")
+# Pkg.add("DataStructures")
+# Pkg.add("Revise")
+# #Pkg.add("Plots")
+# #Pkg.add("Plotly")
+# #Pkg.add("Gadfly")
+# Pkg.add("NaNMath")
 
 
 
